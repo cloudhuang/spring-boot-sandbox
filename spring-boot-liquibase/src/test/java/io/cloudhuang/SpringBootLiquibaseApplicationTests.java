34043182855655219a6687703a6279a32f8b39ea
Liping Huang <huanglp@greatld.com>
2019-01-02 16:03:15 +0800

    update unit test

--- spring-boot-liquibase/src/test/java/io/cloudhuang/SpringBootLiquibaseApplicationTests.java
+++ spring-boot-liquibase/src/test/java/io/cloudhuang/SpringBootLiquibaseApplicationTests.java
@@ -1,16 +1,26 @@
 package io.cloudhuang;
 
+import io.cloudhuang.repository.UserRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.core.Is.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootLiquibaseApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
 	public void contextLoads() {
+		int size = userRepository.findAll().size();
+		Assert.assertThat(size, is(4));
 	}
 
 }
